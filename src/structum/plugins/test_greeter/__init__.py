9b0a57db88f12884cb71714500213cf1a81f0dc0
PythonWoods <gianluca.catalano@gmail.com>
2025-12-06 19:22:35 +0100

    chore: remove test-greeter plugin (replaced by test-runner)

--- src/structum/plugins/test_greeter/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-"""test-greeter Plugin Package."""
-
-from .plugin import TestGreeterPlugin
-
-__all__ = ["TestGreeterPlugin"]
--- src/structum/plugins/test_greeter/core/logic.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-"""test-greeter Plugin Core Logic."""
-
-
-def example_function() -> str:
-    """Example function."""
-    return "Hello from test-greeter plugin!"
